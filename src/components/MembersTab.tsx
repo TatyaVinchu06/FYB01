96734709980a795be2437ad0f916a457de0b9b22
Vinchu06 <obhamare27@gmail.com>
2025-08-29 13:11:53 +0530

    Update MembersTab.tsx

--- src/components/MembersTab.tsx
+++ src/components/MembersTab.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
 import { Plus, UserCheck, UserX, DollarSign, Trash2, Save, Edit, Settings } from "lucide-react";
-import { fundService } from "@/lib/fundService";
+import { firestoreService, Transaction } from "@/lib/firestore";
 
 
 
